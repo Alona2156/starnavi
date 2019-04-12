977cd89f2679af5cae5bbd496a9d4f935890f889
Alona <alona@alona.com>
2019-04-12 22:50:41 +0300

    + extract css + source maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ var path = require('path')
 var webpack = require('webpack')
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: './src/main.js',
@@ -38,7 +40,8 @@ module.exports = {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
-          loaders: {
+          extractCSS: true
+          /*loaders: {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
@@ -52,7 +55,7 @@ module.exports = {
               'css-loader',
               'sass-loader?indentedSyntax'
             ]
-          }
+          }*/
           // other vue-loader options go here
         }
       },
@@ -74,7 +77,12 @@ module.exports = {
     new HTMLWebpackPlugin({
       template: "index.html"
     }),
-    new CleanWebpackPlugin(['dist'])
+    new CleanWebpackPlugin(['dist']),
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'common',
+        filename: 'commons.js'
+    }),
+    new ExtractTextPlugin("style.css")
   ],
   resolve: {
     alias: {
@@ -90,11 +98,11 @@ module.exports = {
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map'
+  devtool: 'source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = ""
+  //module.exports.devtool = ""
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
